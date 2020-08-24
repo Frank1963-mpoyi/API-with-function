62f7b0cd957eebc52b998b2b0dee23fbd6559e5c
mpoyi <frankmpoyi63@gmail.com>
2020-08-24 18:21:17 +0200

    Django_browser_api

--- resto/serializers.py
+++ resto/serializers.py
@@ -7,4 +7,5 @@ from resto.models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id','title','author']
+        #fields = ['id','title','author']
+        fields = '__all__'
--- resto/views.py
+++ resto/views.py
@@ -1,52 +1,64 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, JsonResponse
+#from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+#from rest_framework.parsers import JSONParser
 from .models import Article
 from resto.serializers import ArticleSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response # here not Json response
+from rest_framework import status
 
-@csrf_exempt
+
+
+
+
+# DJANGO BROWSER API
+
+#@csrf_exempt
+@api_view(['GET','POST'])
 def article_list(request):
     # RETRIEVE QUERY SET
     if request.method == 'GET':
         articles = Article.objects.all()
         # here we can serialize the article
         serializer = ArticleSerializer(articles, many = True) # many =True because we serialize the queryset 
-        return JsonResponse(serializer.data, safe=False) #By default, the JsonResponse’s first parameter, data, should be a dict instance. To pass any other JSON-serializable object you must set the safe parameter to False.
-        
+        #return JsonResponse(serializer.data, safe=False) #By default, the JsonResponse’s first parameter, data, should be a dict instance. To pass any other JSON-serializable object you must set the safe parameter to False.
+        return Response(serializer.data)
         # POST METHOD
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(data = data)
+        #data = JSONParser().parse(request)
+        #serializer = ArticleSerializer(data = data)
+        serializer = ArticleSerializer(data = request.data) # we need to get the data from request
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status = 400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED) # you must import this status
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 
 
-@csrf_exempt
+#@csrf_exempt
+@api_view(['GET','PUT','DELETE'])
 def article_detail(request, pk=None):
     # GET METHOD 
-    #article = get_object_or_404(Article, pk = pk)
-    article = Article.objects.get(id=pk)
+    article = get_object_or_404(Article, pk = pk)
+    #article = Article.objects.get(id=pk)
 
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     # UPDATE METHOD
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(article, data = data)
+        #data = JSONParser().parse(request)
+        serializer = ArticleSerializer(article, data = request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status = 400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
         # DELETE METHOD
     elif request.method == 'DELETE':
         article.delete()
-        return HttpResponse(status = 204)
+        return Response(status = status.HTTP_204_NO_CONTENT)
